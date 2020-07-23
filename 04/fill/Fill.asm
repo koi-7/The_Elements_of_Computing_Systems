1a4bcb11bb650a7eed629da7a163865d26360539
koi <kkoiwa.1720+git@gmail.com>
2020-07-23 17:41:53 +0900

    20/07/23 17:41

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,55 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@8192  // 8192(word) = 32(word/row) * 256(row)
+D=A
+@screen_word_size
+M=D
+
+(LOOP)
+    @SCREEN
+    D=A
+    @fill_address
+    M=D
+
+    @KBD
+    D=M
+    @Key_pressed
+    D;JGT
+
+// キーボード非押下（color: 白に設定）
+    @color
+    M=0
+    @Fill_screen
+    0;JMP
+
+// キーボード押下（color: 黒に設定）
+(Key_pressed)
+    @color
+    M=-1
+
+// 設定された色でスクリーンを塗る
+(Fill_screen)
+    // 色とアドレスを指定して着色
+    @color
+    D=M
+    @fill_address
+    A=M
+    M=D
+
+    // 次のアドレスへ
+    @fill_address
+    M=M+1
+
+    // 色を塗り終えるまでループ
+    D=M
+    @SCREEN
+    D=D-A
+    @screen_word_size
+    D=D-M
+    @Fill_screen
+    D;JLT
+
+    @LOOP
+    0;JEQ
--- 04/fill/README.md
+++ 04/fill/README.md
@@ -1,2 +1,10 @@
 # 4章 機械語（入出力操作プログラム ```Fill.asm```）
 
+## 処理のイメージ
+
+![Fill](https://user-images.githubusercontent.com/61448492/88266736-a9c74f80-cd0a-11ea-8111-113fbc7cf922.png)
+
+## その他
+
+- 色を黒にするときは -1 に設定する（先人の知恵）
+- CPU エミュレータを使用する時は上部にある ```Animate``` の設定を ```No Animation``` にする
