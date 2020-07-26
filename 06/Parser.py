8d2e5c71c8d055cf6186e9f1c87a66a76f84860f
koi <kkoiwa.1720+git@gmail.com>
2020-07-26 16:08:20 +0900

    6章の.asm追加

--- 06/Parser.py
+++ 06/Parser.py
@@ -2,4 +2,61 @@
 # coding: utf-8
 
 class Parser:
-    pass
+    def __init__(self, filename):
+        pass
+
+    def hasMoreCommands(self):
+        '''
+        入力にまだコマンドが存在するか？
+        in:  void
+        out: bool
+        '''
+        pass
+
+    def advance(self):
+        '''
+        入力から次のコマンドを読み、それを現在のコマンドにする
+        in:  void
+        out: void
+        '''
+        pass
+
+    def commandType(self):
+        '''
+       現コマンドの種類を返す
+        in:  void
+        out: COMMAND
+        '''
+        pass
+
+    def symbol(self):
+        '''
+        現コマンドのシンボルを返す
+        in:  void
+        out: str
+        '''
+        pass
+
+    def dest(self):
+        '''
+        現 C 命令の dest ニーモニックを返す
+        in:  void
+        out: str
+        '''
+        pass
+
+    def comp(self):
+        '''
+        現 C 命令の comp ニーモニックを返す
+        in:  void
+        out: str
+        '''
+        pass
+
+    def jump(self):
+        '''
+        現 C 命令の jump ニーモニックを返す
+        in:  void
+        out: str
+        '''
+        pass
