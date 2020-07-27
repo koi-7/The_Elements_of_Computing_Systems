6a0963cef62785ce48d90548b48794b884f56d1b
koi <kkoiwa.1720+git@gmail.com>
2020-07-27 18:18:45 +0900

    シンボルテーブルの作成まで

--- 06/SymbolTable.py
+++ 06/SymbolTable.py
@@ -2,4 +2,54 @@
 # coding: utf-8
 
 class SymbolTable:
-    pass
+    def __init__(self):
+        '''
+        空のシンボルテーブルを作成する
+        void -> void
+        '''
+        self.table = {
+            'SP':     0,
+            'LCL':    1,
+            'ARG':    2,
+            'THIS':   3,
+            'THAT':   4,
+            'R0':     0,
+            'R1':     1,
+            'R2':     2,
+            'R3':     3,
+            'R4':     4,
+            'R5':     5,
+            'R6':     6,
+            'R7':     7,
+            'R8':     8,
+            'R9':     9,
+            'R10':    10,
+            'R11':    11,
+            'R12':    12,
+            'R13':    13,
+            'R14':    14,
+            'R15':    15,
+            'SCREEN': 16384,
+            'KBD':    24576,
+        }
+
+    def addEntry(self, symbol, address):
+        '''
+        テーブルに (symbol, address) のペアを追加する
+        str, int -> void
+        '''
+        self.table[symbol] = address
+
+    def contains(self, symbol):
+        '''
+        シンボルテーブルは与えられた symbol を含むか？
+        str -> bool
+        '''
+        return symbol in self.table
+
+    def getAddress(self, symbol):
+        '''
+        symbol に結びつけられたアドレスを返す
+        str -> int
+        '''
+        return self.table[symbol]
--- 06/main.py
+++ 06/main.py
@@ -4,23 +4,44 @@
 import sys, os
 import Parser as ps
 import Code as cd
+import SymbolTable as st
+
+START_ADDRESS = 16
 
 def main():
     filename = sys.argv[1]
     outfile = os.path.splitext(filename)[0] + '.hack'
 
+    t = st.SymbolTable()
+    address_counter = START_ADDRESS
+
+    ## 1 周目（シンボルテーブルの作成）
+    with ps.Parser(filename) as p:
+        while p.hasMoreCommands():
+            p.advance()
+            if p.commandType() != ps.C_COMMAND:
+                s = p.symbol()
+                if (not str.isdecimal(s)) & (not t.contains(s)):
+                    t.addEntry(s, address_counter)
+                    address_counter += 1
+
+    print(t.table)
+    exit()
+    ########## とりあえずここまで ##########
+
+    ## 2 周目（.hack ファイルの生成）
     with open(outfile, 'wt') as fout:
         with ps.Parser(filename) as p:
             while p.hasMoreCommands():
                 p.advance()
                 print('command: ' + p.command + ' (type: ' + str(p.commandType()) + ')')
-                if p.commandType() != ps.C_COMMAND:          ## @Xxx or (Xxx)
+                if p.commandType() != ps.C_COMMAND:  ## @Xxx or (Xxx)
                     s = p.symbol()
-                    if True:          ## 定数
+                    if s.isdecimal():  ## 定数
                         bin = format(int(s), '016b')
-                    else:             ## シンボル
-                        pass
-                else:                                        ## C 命令
+                    else:              ## シンボル
+                        bin = format(t.getAddress(s), '016b')
+                else:                                ## C 命令
                     bin_comp = cd.Code().comp(p.comp())
                     bin_dest = cd.Code().dest(p.dest())
                     bin_jump = cd.Code().jump(p.jump())
