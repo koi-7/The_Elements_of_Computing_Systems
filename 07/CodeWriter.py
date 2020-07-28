5e8b459a5bf9184fe52a169809f36a8551c62a4e
koi <kkoiwa.1720+git@gmail.com>
2020-07-28 18:28:07 +0900

    20/07/28 18:28 途中

--- 07/CodeWriter.py
+++ 07/CodeWriter.py
@@ -1,2 +1,38 @@
 #!/usr/bin/python3
 # coding: utf-8
+
+class CodeWriter:
+    def __init__(self, filename):
+        '''
+        出力ファイル / ストリームを開き、書き込む準備を行う
+        str -> void
+        '''
+        self.f = open(filename, 'wt')
+
+    def setFileName(self, fileName):
+        '''
+        CodeWriter モジュールに新しい VM ファイルの変換が開始したことを知らせる
+        str -> void
+        '''
+        pass
+
+    def writeArithmetic(self, command):
+        '''
+        与えられた算術コマンドをアセンブリコードに変換し、それを書き込む
+        str -> void
+        '''
+        pass
+
+    def writePushPop(self, command, segment, index):
+        '''
+        C_PUSH または C_POP コマンドをアセンブリコードに変換し、それを書き込む
+        int, str, int -> void
+        '''
+        pass
+
+    def close(self):
+        '''
+        出力ファイルを閉じる
+        void -> void
+        '''
+        self.f.close()
--- 07/main.py
+++ 07/main.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import sys, os
+import Parser as ps
+import CodeWriter as cw
+
 def main():
+    filename = sys.argv[1]
+    outfile = os.path.splitext(filename)[0] + '.asm'
+    with ps.Parser(filename) as p:
+        while p.hasMoreCommands():
+            p.advance()
 
 if __name__=='__main__':
     main()
