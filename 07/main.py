2ba0df96325ba5cda3a650bac667b0fcdb042685
koi <kkoiwa.1720+git@gmail.com>
2020-07-29 12:19:55 +0900

    20/07/29 12:19 SimpleAdd 途中

--- 07/main.py
+++ 07/main.py
@@ -9,8 +9,14 @@ def main():
     filename = sys.argv[1]
     outfile = os.path.splitext(filename)[0] + '.asm'
     with ps.Parser(filename) as p:
+        c = cw.CodeWriter(outfile)
         while p.hasMoreCommands():
             p.advance()
+            if p.commandType() == ps.C_PUSH or p.commandType() == ps.C_POP:
+                com = p.command.split(' ')[0]
+                c.writePushPop(com, p.arg1(), p.arg2())
+            elif p.commandType() == ps.C_ARITHMETIC:
+                c.writeArithmetic(p.command)
 
 if __name__=='__main__':
     main()
