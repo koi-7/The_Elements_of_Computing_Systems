d2405092076e7ed109ab6a19fe7169b63afe71a3
koi <kkoiwa.1720+git@gmail.com>
2020-08-05 11:52:26 +0900

    トークナイザ途中

--- 10/JackAnalyzer.py
+++ 10/JackAnalyzer.py
@@ -1,8 +1,38 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import sys, os, glob
+import JackTokenizer as jt
+import CompilationEngine as ce
+
 def main():
-    pass
+    path = sys.argv[1]
+
+    if os.path.isfile(path):
+        file_list = [sys.argv[1]]
+        #output_file = os.path.splitext(path)[0] + '.xml'
+    else:
+        if path[-1] != '/':
+            path = path + '/'
+        file_list = glob.glob(path + '*.jack')
+        #output_file = path + path.split('/')[-2] + '.asm'
+        ## Sys.vm を file_list の先頭に
+        #index_of_sys = file_list.index(path + 'Sys.vm')
+        #file_list[0], file_list[index_of_sys] = \
+        #    file_list[index_of_sys], file_list[0]
+
+    for input_file in file_list:
+        j = jt.JackTokenizer(input_file)
+        output_file = input_file.replace('.jack', 'T.xml')
+        c = ce.CompilationEngine(input_file, output_file)
+        while j.hasMoreTokens():
+            pass
+
+
+
+
+
+        j.f.close()
 
 if __name__=='__main__':
     main()
--- 10/JackTokenizer.py
+++ 10/JackTokenizer.py
@@ -1,20 +1,79 @@
 #!/usr/bin/python3
 # coding: utf-8
 
+import re
+
+KEYWORD      = 0
+SYMBOL       = 1
+IDENTIFIER   = 2
+INT_CONST    = 3
+STRING_CONST = 4
+
+CLASS       = 5
+METHOD      = 6
+FUNCTION    = 7
+CONSTRUCTOR = 8
+INT         = 9
+BOOLEAN     = 10
+CHAR        = 11
+VOID        = 12
+VAR         = 13
+STATIC      = 14
+FIELD       = 15
+LET         = 16
+DO          = 17
+IF          = 18
+ELSE        = 19
+WHILE       = 20
+RETURN      = 21
+TRUE        = 22
+FALSE       = 23
+NULL        = 24
+THIS        = 25
+
+keyword_dict = {
+    'class': CLASS, 'constructor': CONSTRUCTOR, 'function': FUNCTION,
+    'method': METHOD, 'field': FIELD, 'static': STATIC, 'var': VAR, 'int': INT,
+    'char': CHAR, 'boolean': BOOLEAN, 'void': VOID, 'true': TRUE, 'false': FALSE,
+    'null': NULL, 'this': THIS, 'let': LET, 'do': DO, 'if': IF, 'else': ELSE,
+    'while': WHILE, 'return': RETURN,
+}
+
+symbol_set = {
+    '{', '}', '(', ')', '[', ']', '.', ',', ';', '+',
+    '-', '*', '/', '&', '|', '<', '>', '=', '~'
+}
+
+indent_level = 0
+
 class JackTokenizer:
     def __init__(self, input_file):
         '''
         入力ファイル / ストリームを開き、トークン化を行う準備をする
         str -> void
         '''
-        pass
+        self.token = ''
+        self.token_list = []
+        self.f = open(input_file, 'rt')
 
     def hasMoreTokens(self):
         '''
         入力にまだトークンは存在するか？
         void -> bool
         '''
-        pass
+        while True:
+            line = self.f.readline()
+            if line == '':                     ## ファイルの終端
+                return False
+            elif re.match(r'^\s*\n$|^/', line):  ## 改行のみもしくはコメントのみ
+                continue
+            else:                              ## コマンドを含む行
+                # TODO: トークンリストを作って True を返す
+                line = line.split('//')[0]
+                line = line.strip()
+                list = line.split(' ')
+                print(list)
+                return True
 
     def advance(self):
         '''
@@ -22,14 +81,23 @@ class JackTokenizer:
         hasMoreTokens が True の場合のみ呼び出すことができる
         void -> void
         '''
-        pass
+        self.token = self.token_list[0]
 
     def tokenType(self):
         '''
         現トークンの種類を返す
         void -> KEYWORD | SYMBOL | IDENTIFIER | INT_CONST | STRING_CONST
         '''
-        pass
+        if self.token in keyword_dict:
+            return KEYWORD
+        elif self.token in symbol_set:
+            return SYMBOL
+        elif self.token.isdecimal():
+            return INT_CONST
+        elif self.token[0] == '"':
+            return STRING_CONST
+        else:
+            return IDENTIFIER
 
     def keyWord(self):
         '''
@@ -39,7 +107,7 @@ class JackTokenizer:
                 VOID | VAR | STATIC | FIELD | LET | DO | IF | ELSE | WHILE |
                 RETURN | TRUE | FALSE | NULL | THIS
         '''
-        pass
+        return keyword_dict[self.token]
 
     def symbol(self):
         '''
@@ -47,7 +115,7 @@ class JackTokenizer:
         tokenType() が SYMBOL の場合のみ呼び出すことができる
         void -> str
         '''
-        pass
+        return self.token
 
     def identifier(self):
         '''
@@ -55,7 +123,7 @@ class JackTokenizer:
         tokenType() が IDENTIFIER の場合のみ呼び出すことができる
         void -> str
         '''
-        pass
+        return self.token
 
     def intVal(self):
         '''
@@ -63,7 +131,7 @@ class JackTokenizer:
         tokenType() が INT_CONST の場合のみ呼び出すことができる
         void -> int
         '''
-        pass
+        return int(self.token)
 
     def stringVal(self):
         '''
@@ -71,4 +139,4 @@ class JackTokenizer:
         tokenType() が STRING_CONST の場合のみ呼び出すことができる
         void -> str
         '''
-        pass
+        return self.token.strip('"')
